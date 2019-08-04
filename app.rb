445b451595bdfbd19bc275a732abef10e3cd69bc
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-04 00:00:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
   get '/new' do
     erb :create_puppy
   end
-
-  post "/puppy" do
+ 
+  post '/puppy' do
     @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
   end
